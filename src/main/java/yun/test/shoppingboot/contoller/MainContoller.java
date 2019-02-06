ce7e8448b668f88fbe158af7286aed8afc79386d
DESKTOP-1NBEG5A\user <cjswo9207@naver.com>
2019-02-06 20:08:30 +0900

    docs : 쇼핑몰 포트폴리오

--- src/main/java/yun/test/shoppingboot/contoller/MainContoller.java
+++ src/main/java/yun/test/shoppingboot/contoller/MainContoller.java
@@ -6,15 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class MainContoller  {
+public class MainContoller {
 
-    @GetMapping(value = {"/","main"})
-    public String main(Model model){
+    @GetMapping(value = {"/", "main"})
+    public String main(Model model) {
         return "main";
     }
 
-    @PostMapping(value = {"/","main"})
-    public String searchMain(Model model){
+    @PostMapping(value = {"/", "main"})
+    public String searchMain(Model model) {
         return "main";
     }
+
+    // TODO
+//    @ResponseBody
+//    @RequestMapping(value="/customer/eventViewGet.json")
+//    public CustomerVO eventViewGet(ModelMap model, HttpServletRequest ret,  HttpServletResponse req, CustomerVO customerVO, @RequestParam("seq") String seq)
+//            throws Exception {
+//
+//        CustomerVO result = new CustomerVO();
+//        customerVO.setSeq(seq);
+//        result=customerService.eventViewGet(customerVO);
+//
+//        System.out.println(result.getKeywords());
+//
+//        return result;
+//    }
 }
